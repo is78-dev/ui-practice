06e99560fb4e2222bb1759be134da582ab1486b2
is1-coder <isoshi.desktop@gmail.com>
2025-04-27 02:29:12 +0900

    fix: 遷移先urlにbaseを含める

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -11,8 +11,8 @@ export default defineConfig({
   },
 
   integrations: [react(), vue()],
-  site: "https://is78-dev.github.io/ui-practice",
-  base: "/ui-practice/",
+  site: "https://is78-dev.github.io",
+  base: "/ui-practice",
   outDir: "./dist",
   output: "static",
 });
---
+++ src/utils/getBaseUrl.ts
@@ -0,0 +1,4 @@
+export const getAbsoluteUrl = (url: string) => {
+  const baseUrl = import.meta.env.BASE_URL;
+  return `${baseUrl}/${url}`;
+};
